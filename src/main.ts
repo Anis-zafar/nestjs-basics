65b429d463b69792a0bb24bdda71e11a2034ed0a
anis-zafar <anis.zafar@ceative.co.uk>
2023-03-09 18:27:38 +0500

    commit

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { UsersModule } from './users/users.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,8 +11,12 @@ async function bootstrap() {
     .setDescription('The user API')
     .setVersion('1.0.0')
     .addBearerAuth()
+    // .addTag('User')
+    // .addTag('emp')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, config, {
+    include: [UsersModule],
+  });
   SwaggerModule.setup('api', app, document);
 
   const port = 3000;
